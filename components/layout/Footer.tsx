e79a106a3610e20fd531c105b7c06c35c3fd84be
zeynepdundar <zdundar@lingaros.com>
2026-02-01 01:11:32 +0300

    refactor(footer): added one more paragraph to redirect source code

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,11 +4,46 @@ export default function Footer() {
 
   return (
     <footer className="mt-24">
-      <div className="mx-auto max-w-6xl px-6">
-        {/* Copyright */}
-        <p className="py-6 text-center text-sm text-muted">
-          © {new Date().getFullYear()} Zeynep Dundar. All rights reserved.
+      <div className="mx-auto max-w-6xl px-6 flex flex-col items-center gap-4 text-sm text-muted">
+        <p>
+          © {new Date().getFullYear()} Zeynep Dündar. All rights reserved.
         </p>
+        <p className="text-muted">
+          Built with{" "}
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="  text-foreground/70
+  hover:text-foreground
+  transition
+  hover:translate-x-0.5"
+          >
+            Next.js
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://vercel.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="  text-foreground/70
+  hover:text-foreground
+  transition
+  hover:translate-x-0.5"
+          >
+            Vercel
+          </a>
+          . Source code available on{" "}
+          <a
+            href="https://github.com/zeynepdundar/PortfolioV2"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-foreground/70 hover:text-foreground transition"
+          >
+            GitHub
+          </a>.
+        </p>
+
       </div>
     </footer>
   );
